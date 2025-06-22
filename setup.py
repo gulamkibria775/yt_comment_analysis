3b09e9ca1153d05996e7922c9a3b355a5a3701e8
gulamkibria775 <gulamkibria775@gmail.com>
2025-06-22 21:16:58 +0600

    git connect

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a short project helping a youtuber and content creator',
+    author='gulam kibria',
+    license='',
+)
